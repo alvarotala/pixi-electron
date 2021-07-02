f9a6ad8f0758b76f7a3223494c85959da3d40549
Alvaro <alvarotala@gmail.com>
2021-07-02 12:40:53 -0400

    read www files from: /home/alvarotala/cfdist/index.html

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'bridge.js')
     }
-  })
+  });
 
-  mainWindow.loadFile(path.join(__dirname, 'www', 'index.html'));
+  mainWindow.loadFile('/home/alvarotala/cfdist/index.html');
+
+  /// mainWindow.loadFile(path.join(__dirname, 'www', 'index.html'));
 
   /// test from ubunntu core..
   // mainWindow.loadURL('http://192.168.100.5:8080')
@@ -26,7 +28,7 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
-app.whenReady().then(() => createWindow())
+app.whenReady().then(() => createWindow());
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
